ab6504946fcc3a6002f358dc4008763b501e2935
YanzXz1 <106466639+YanzXz1@users.noreply.github.com>
2022-06-12 14:46:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,8 +18,8 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Yanz Komcol" //namabot kalian
-global.ownername= "Yanz Elek" //nama kalian
+global.botname = "Yanz Tiji" //namabot kalian
+global.ownername= "Yanz Xz" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
@@ -31,8 +31,8 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = [global.ownernomer = "+6285641164344","6285641164344"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Yanz Elek' //sticker wm ubah
-global.author = 'Di Buat Oleh Yanz Clmk' //sticker wm ganti nama kalian
+global.packname = '© Yanz Tiji' //sticker wm ubah
+global.author = 'Di Buat Oleh Yanz Cees++' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
